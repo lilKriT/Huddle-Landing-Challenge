77b6bc8b32b1b0a3cafbe20ff17d25abbcc61cf7
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-26 23:55:35 +0100

    Working on footer.

--- src/elements/h-footer.js
+++ src/elements/h-footer.js
@@ -1,4 +1,5 @@
 import { LitElement, css, html } from "lit";
+import logo from "/src/assets/images/logo.svg";
 
 export class HFooter extends LitElement {
   static get properties() {
@@ -13,11 +14,57 @@ export class HFooter extends LitElement {
   }
 
   render() {
-    return html`<h2>Footer here</h2>`;
+    return html`
+      <footer>
+        <div className="wrapper">
+          <img src="${logo}" />
+
+          <div className="content">
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua
+
+              <a href="#">+1-543-123-4567</a>
+              <a href="#">example@huddle.com</a>
+            </div>
+
+            <div>
+              <a href="#">About Us</a>
+              <a href="#">What We Do</a>
+              <a href="#">FAQ</a>
+            </div>
+
+            <div>
+              <a href="#">Career</a>
+              <a href="#">Blog</a>
+              <a href="#">Contact Us</a>
+            </div>
+
+            <div>
+              <a href="#">+1-543-123-4567</a>
+              <a href="#">+1-543-123-4567</a>
+              <a href="#">+1-543-123-4567</a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    `;
   }
 
   static get styles() {
-    return css``;
+    return css`
+      footer {
+        background-color: var(--veryDarkCyan);
+        color: #fff;
+      }
+
+      .wrapper {
+        max-width: var(--width);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+    `;
   }
 }
 
